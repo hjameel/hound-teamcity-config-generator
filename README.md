a1ce0a592b6e972fe1f8151a6e703b1b6886ba5e
Darrell Mozingo <darrell@mozingo.net>
2015-09-25 11:59:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Generates a config for Etsy's [Hound](https://github.com/etsy/hound), crawling over a list of TeamCity servers to get their VCS roots and spitting it out. Archived projects are ignored.
+
+Usage:
+1. `bundle install`
+1. `touch serverlist`
+1. Add TeamCity server hostnames to `serverlist`, each on a new line
+1. `./generate-config.rb >> config.json`
+1. Put `config.json` in your Hound folder
---
+++ generate-config.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require 'faraday'
+require 'json'
+
+teamcity_hosts=File.readlines('serverlist').each{|server| server.chop!}
+config_body = { 'dbpath' => '../Hound-data', 'repos' => {} }
+
+teamcity_hosts.each do |teamcity_host|
+  roots = JSON.parse((Faraday.get "http://#{teamcity_host}/guestAuth/app/rest/vcs-roots", {}, {'Accept' => 'application/json'}).body)
+
+  roots['vcs-root'].each do |root|
+    root_info = JSON.parse((Faraday.get "http://#{teamcity_host}#{root['href']}", {}, {'Accept' => 'application/json'}).body)
+
+    if (root_info['project'] && root_info['project']['archived'])
+      next
+    end
+
+    name = root_info['project'] ? root_info['project']['name'] : root_info['name']
+    git_url = (root_info['properties']['property'].select { |p| p['name'] == 'url' })[0]['value']
+
+    repo_already_added = (config_body['repos'].select { |key, repo| repo['url'] == git_url }).empty?
+
+    if (!repo_already_added)
+      next
+    end
+
+    config_body['repos'][name] = {'url' => git_url}
+  end
+end
+
+puts JSON.pretty_generate(config_body)
